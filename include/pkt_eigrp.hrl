f38f8c3db2d0cad19835733f1eedeebfdef6c59f
Stephen Han <stephen.han@hmatix.com>
2019-02-20 19:44:59 -0800

    HMXOS-1143 EIGRP(88) protocol support

--- include/pkt_eigrp.hrl
+++ include/pkt_eigrp.hrl
@@ -3,8 +3,8 @@
     opcode :: pkt:uint8_t(),
     sum :: pkt:uint16_t(),
     flags :: pkt:uint32_t(),
-    seq :: pkt:uint32_t(),
-    ack :: pkt:uint32_t(),
-    id :: pkt:uint16_t(),
-    sysnum :: pkt:uint16_t()
+    seq_num :: pkt:uint32_t(),
+    ack_num :: pkt:uint32_t(),
+    router_id :: pkt:uint16_t(),
+    sys_num :: pkt:uint16_t()
 }).
